a82f37fd7e1dee16b94e5046e87819fdb9ebb7c3
Daniel Parizotto <parizottojs@gmail.com>
2023-01-16 22:20:44 -0300

    First commit Project Base, components without content

---
+++ src/pages/HomePage/RecentWork/index.tsx
@@ -0,0 +1,3 @@
+export default function RecentWork() {
+  return <div>RecentWork</div>;
+}
---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,32 @@
+import Home from "./Home";
+import Story from "./Story";
+import Services from "./Services";
+import Promo from "./Promo";
+import RecentWork from "./RecentWork";
+import Pricing from "./Pricing";
+import Team from "./Team";
+import Solutions from "./Solutions";
+import Testimonials from "./Testimonials";
+import News from "./News";
+import Clients from "./Clients";
+import Contact from "./Contact";
+import Footer from "./Footer";
+export default function HomePage() {
+  return (
+    <div>
+      <Home />
+      <Story />
+      <Services />
+      <Promo />
+      <RecentWork />
+      <Pricing />
+      <Team />
+      <Solutions />
+      <Testimonials />
+      <News />
+      <Clients />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
